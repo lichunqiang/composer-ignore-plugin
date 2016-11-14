82ef834f36ceb7c6fe5048810f2a99d5e1386a67
lichunqiang <light-li@hotmail.com>
2016-11-14 15:54:55 +0800

    initialize

---
+++ Plugin.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Light\Composer;
+
+use Composer\Composer;
+use Composer\Config;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\IO\IOInterface;
+use Composer\Package\Package;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use Composer\Util\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * The composer ignore plugin.
+ *
+ * @author lichunqaing<light-li@hotmail.com>
+ * @version 1.0.0
+ * @license MIT
+ */
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+	/**
+	 * @var Composer
+	 */
+	protected $composer;
+	/**
+	 * @var Config
+	 */
+	protected $config;
+	/**
+	 * @var Filesystem
+	 */
+	protected $fileSystem;
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function activate(Composer $composer, IOInterface $io)
+	{
+		$this->composer = $composer;
+		$this->config = $composer->getConfig();
+		$this->fileSystem = new Filesystem();
+	}
+	
+	/**
+	 * @inheritdoc
+	 */
+	public static function getSubscribedEvents()
+	{
+		return [
+			ScriptEvents::POST_AUTOLOAD_DUMP => 'onPostAutoloadDump'
+		];
+	}
+	
+	/**
+	 * @param Event $event
+	 */
+	public function onPostAutoloadDump(Event $event)
+	{
+		/** @var Package $package */
+		$package = $this->composer->getPackage();
+		$extra =  $package->getExtra();
+		
+		$ignoreList = isset($extra['light-ignore-plugin']) ? $extra['light-ignore-plugin'] : null;
+		
+		if (!$ignoreList) {
+			return;
+		}
+		
+		$vendor_dir = $this->config->get('vendor-dir');
+		
+		foreach ($ignoreList as $vendor => $files) {
+			$root = $this->fileSystem->normalizePath("{$vendor_dir}/{$vendor}");
+			$this->ignorePath($root, $files);
+		}
+	}
+	
+	/**
+	 * @param string $root
+	 * @param array $files
+	 */
+	protected function ignorePath($root, array $files)
+	{
+		foreach ($files as $file) {
+			$_file = $this->fileSystem->normalizePath("{$root}/{$file}");
+			if (is_dir($_file)) {
+				$this->fileSystem->removeDirectory($_file);
+			} else {
+				$finder = Finder::create()->in($root)->name($file)->files();
+				
+				foreach ($finder as $item) {
+					$this->fileSystem->remove($item->getRealPath());
+				}
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,74 @@
+Composer Ignore Plugin
+----
+
+This plugin help us to remove the unused file or directories in vendor.
+
+## Installation
+
+```
+$ composer global require "light/composer-ignore-plugin:~1.0"
+```
+
+## Usage
+
+Define the ignore file or directory in composer.json, for example:
+
+Before:
+
+```
+fzaninotto/faker/
+├── CHANGELOG.md
+├── composer.json
+├── CONTRIBUTING.md
+├── LICENSE
+├── Makefile
+├── phpunit.xml.dist
+├── readme.md
+├── src
+└── test
+```
+
+Configuration in `composer.json`:
+
+```json
+{
+	"extra": {
+		"light-ignore-plugin": {
+    	  "fzaninotto/faker": [
+    	  	"test", 
+    	  	"*.md", 
+    	  	"LICENSE",
+    	  	"Makefile",
+    	  	"phpunit.xml.dist"
+    	  ]
+    	}
+	}
+}
+
+```
+
+After executed `composer install`, `composer update`, `composer dump-autoload`, The files will be removed.
+
+> When execute the `composer install` or `composer update` will finally trigger the autoload dump event
+
+After:
+
+```
+fzaninotto/faker/
+├── composer.json
+└── src
+```
+
+## Why this?
+
+Thanks to open source, there are many useful packages helped us. 
+
+Generally, some files or folder in the installed package is useless, and when deploy to production system, reduce the files can make deploy clean.
+
+Of cause, a lot of package had done this by add `.gitattributes` file, But also not all, `fzaninotto/faker` for example.
+
+## LICENSE
+
+MIT
+
+
